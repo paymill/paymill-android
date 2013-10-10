c48bee625ba49884ff910eac544b470a9f76045c
Emilian Stoilkov <stoilkov@gmail.com>
2013-10-10 12:06:44 +0200

    up vouchermill docs

---
+++ docs/samples/vouchermill/assets/navtree_data.js
@@ -0,0 +1,8 @@
+var NAVTREE_DATA =
+[ [ "com.paymill.android.payment", "com/paymill/android/payment/package-summary.html", [ [ "Classes", null, [ [ "CardTypeParser", "com/paymill/android/payment/CardTypeParser.html", null, "" ], [ "CreditCardFragment", "com/paymill/android/payment/CreditCardFragment.html", null, "" ], [ "CreditCardValidator", "com/paymill/android/payment/CreditCardValidator.html", null, "" ], [ "CustomEditText", "com/paymill/android/payment/CustomEditText.html", null, "" ], [ "ElvFragment", "com/paymill/android/payment/ElvFragment.html", null, "" ], [ "ElvValidator", "com/paymill/android/payment/ElvValidator.html", null, "" ], [ "PaymentActivity", "com/paymill/android/payment/PaymentActivity.html", null, "" ], [ "PaymentActivity.Factory", "com/paymill/android/payment/PaymentActivity.Factory.html", null, "" ], [ "PaymentActivity.Result", "com/paymill/android/payment/PaymentActivity.Result.html", null, "" ], [ "PaymentActivity.Settings", "com/paymill/android/payment/PaymentActivity.Settings.html", null, "" ], [ "ProgressDialogFragment", "com/paymill/android/payment/ProgressDialogFragment.html", null, "" ] ]
+, "" ], [ "Enums", null, [ [ "CardTypeParser.CardType", "com/paymill/android/payment/CardTypeParser.CardType.html", null, "" ], [ "PaymentActivity.PaymentType", "com/paymill/android/payment/PaymentActivity.PaymentType.html", null, "" ], [ "PaymentActivity.Result.Type", "com/paymill/android/payment/PaymentActivity.Result.Type.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
---
+++ docs/samples/vouchermill/reference/lists.js
@@ -0,0 +1,18 @@
+var DATA = [
+      { id:0, label:"com.paymill.android.payment", link:"com/paymill/android/payment/package-summary.html", type:"package" },
+      { id:1, label:"com.paymill.android.payment.CardTypeParser", link:"com/paymill/android/payment/CardTypeParser.html", type:"class" },
+      { id:2, label:"com.paymill.android.payment.CardTypeParser.CardType", link:"com/paymill/android/payment/CardTypeParser.CardType.html", type:"class" },
+      { id:3, label:"com.paymill.android.payment.CreditCardFragment", link:"com/paymill/android/payment/CreditCardFragment.html", type:"class" },
+      { id:4, label:"com.paymill.android.payment.CreditCardValidator", link:"com/paymill/android/payment/CreditCardValidator.html", type:"class" },
+      { id:5, label:"com.paymill.android.payment.CustomEditText", link:"com/paymill/android/payment/CustomEditText.html", type:"class" },
+      { id:6, label:"com.paymill.android.payment.ElvFragment", link:"com/paymill/android/payment/ElvFragment.html", type:"class" },
+      { id:7, label:"com.paymill.android.payment.ElvValidator", link:"com/paymill/android/payment/ElvValidator.html", type:"class" },
+      { id:8, label:"com.paymill.android.payment.PaymentActivity", link:"com/paymill/android/payment/PaymentActivity.html", type:"class" },
+      { id:9, label:"com.paymill.android.payment.PaymentActivity.Factory", link:"com/paymill/android/payment/PaymentActivity.Factory.html", type:"class" },
+      { id:10, label:"com.paymill.android.payment.PaymentActivity.PaymentType", link:"com/paymill/android/payment/PaymentActivity.PaymentType.html", type:"class" },
+      { id:11, label:"com.paymill.android.payment.PaymentActivity.Result", link:"com/paymill/android/payment/PaymentActivity.Result.html", type:"class" },
+      { id:12, label:"com.paymill.android.payment.PaymentActivity.Result.Type", link:"com/paymill/android/payment/PaymentActivity.Result.Type.html", type:"class" },
+      { id:13, label:"com.paymill.android.payment.PaymentActivity.Settings", link:"com/paymill/android/payment/PaymentActivity.Settings.html", type:"class" },
+      { id:14, label:"com.paymill.android.payment.ProgressDialogFragment", link:"com/paymill/android/payment/ProgressDialogFragment.html", type:"class" }
+
+    ];
