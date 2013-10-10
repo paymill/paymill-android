49e43a1a1838f89ca745db557f03ef1c41f16a1c
Emilian Stoilkov <stoilkov@gmail.com>
2013-10-10 12:25:20 +0200

    up sdk docs

---
+++ docs/sdk/assets/navtree_data.js
@@ -0,0 +1,16 @@
+var NAVTREE_DATA =
+[ [ "com.paymill.android.api", "com/paymill/android/api/package-summary.html", [ [ "Classes", null, [ [ "Client", "com/paymill/android/api/Client.html", null, "" ], [ "Payment", "com/paymill/android/api/Payment.html", null, "" ], [ "Preauthorization", "com/paymill/android/api/Preauthorization.html", null, "" ], [ "Refund", "com/paymill/android/api/Refund.html", null, "" ], [ "Subscription", "com/paymill/android/api/Subscription.html", null, "" ], [ "Transaction", "com/paymill/android/api/Transaction.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "com.paymill.android.factory", "com/paymill/android/factory/package-summary.html", [ [ "Interfaces", null, [ [ "PMPaymentMethod", "com/paymill/android/factory/PMPaymentMethod.html", null, "" ], [ "PMPaymentParams", "com/paymill/android/factory/PMPaymentParams.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "PMFactory", "com/paymill/android/factory/PMFactory.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "com.paymill.android.listener", "com/paymill/android/listener/package-summary.html", [ [ "Interfaces", null, [ [ "PMBackgroundListener", "com/paymill/android/listener/PMBackgroundListener.html", null, "" ], [ "PMConsumePreauthListener", "com/paymill/android/listener/PMConsumePreauthListener.html", null, "" ], [ "PMConsumeTransListener", "com/paymill/android/listener/PMConsumeTransListener.html", null, "" ], [ "PMGenerateTokenListener", "com/paymill/android/listener/PMGenerateTokenListener.html", null, "" ], [ "PMGetPreauthListener", "com/paymill/android/listener/PMGetPreauthListener.html", null, "" ], [ "PMGetTransListener", "com/paymill/android/listener/PMGetTransListener.html", null, "" ], [ "PMInitListener", "com/paymill/android/listener/PMInitListener.html", null, "" ], [ "PMListNotConsPreauthsListener", "com/paymill/android/listener/PMListNotConsPreauthsListener.html", null, "" ], [ "PMListNotConsTransListener", "com/paymill/android/listener/PMListNotConsTransListener.html", null, "" ], [ "PMListPreauthsListener", "com/paymill/android/listener/PMListPreauthsListener.html", null, "" ], [ "PMListTransListener", "com/paymill/android/listener/PMListTransListener.html", null, "" ], [ "PMNewDeviceIdListener", "com/paymill/android/listener/PMNewDeviceIdListener.html", null, "" ], [ "PMPreauthListener", "com/paymill/android/listener/PMPreauthListener.html", null, "" ], [ "PMTransListener", "com/paymill/android/listener/PMTransListener.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "com.paymill.android.service", "com/paymill/android/service/package-summary.html", [ [ "Classes", null, [ [ "PMManager", "com/paymill/android/service/PMManager.html", null, "" ], [ "PMService", "com/paymill/android/service/PMService.html", null, "" ] ]
+, "" ], [ "Enums", null, [ [ "PMError.Type", "com/paymill/android/service/PMError.Type.html", null, "" ], [ "PMService.ServiceMode", "com/paymill/android/service/PMService.ServiceMode.html", null, "" ] ]
+, "" ], [ "Exceptions", null, [ [ "PMError", "com/paymill/android/service/PMError.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
